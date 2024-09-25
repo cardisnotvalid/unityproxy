b14f1220702febc2ca9ee957ea7d0e33633cea87
oyunsky <deadcardinal293@gmail.com>
2024-09-26 03:07:20 +0700

    refactor: split proxy classes into proxy.py and abc.py

--- unityproxy/proxy.py
+++ unityproxy/proxy.py
@@ -1,58 +1,64 @@
-from .parser import parse_line
+from .abc import BaseProxy
+from .utils import parse_line
 
 
-class BaseProxy:
+class Proxy(BaseProxy):
     def __init__(self, host, port, scheme, username=None, password=None):
-        self.host = host
-        self.port = port
-        self.scheme = scheme
-        self.username = username
-        self.password = password
-
-    def __str__(self):
-        return f"Proxy({self.uri})"
-
-    def __repr__(self):
-        return f"Proxy({self.uri})"
-
-    def is_auth(self):
-        return self.username is not None and self.password is not None
+        super().__init__(host, port, scheme, username, password)
+        self._adapter = None
 
     @property
-    def uri(self):
-        auth = f"{self.username}:{self.password}@" if self.is_auth() else ""
-        return f"{self.scheme}://{auth}{self.host}:{self.port}"
+    def adapter(self):
+        if not self._adapter:
+            self._adapter = ProxyAdapter(self)
+        return self._adapter
 
-
-class ProxyAdapter(BaseProxy):
-    def as_requests(self):
-        return {"http": self.uri, "https": self.uri}
-
-    def as_httpx(self):
-        return {"http://": self.uri, "https://": self.uri}
-
-    def as_httpx_line(self):
-        return self.uri
-
-    def as_pyrogram(self):
-        return {"scheme": self.scheme,
-                "hostname": self.host,
-                "port": self.port,
-                "username": self.username,
-                "password": self.password}
-
-
-class Proxy(ProxyAdapter):
     @classmethod
     def from_data(cls, data, default_scheme="http"):
-        return cls(host=data.get("host"),
-                   port=data.get("port"),
-                   scheme=data.get("scheme", default_scheme),
-                   username=data.get("username"),
-                   password=data.get("password"))
+        return cls(
+            host=data.get("host"),
+            port=data.get("port"),
+            scheme=data.get("scheme", default_scheme),
+            username=data.get("username"),
+            password=data.get("password")
+        )
 
     @classmethod
     def from_line(cls, line, default_scheme="http", custom_parser=None):
-        if data := parse_line(line):
+        if custom_parser:
+            data = custom_parser(line)
+        else:
+            data = parse_line(line)
+        if data:
             return cls.from_data(data, default_scheme)
-        raise ValueError("failed to parse line")
+        raise ValueError(f"Failed to parse line: {line}")
+
+
+class ProxyAdapter:
+    def __init__(self, proxy):
+        self._proxy = proxy
+
+    @property
+    def requests(self):
+        return {
+            "http": self._proxy.uri,
+            "https": self._proxy.uri
+        }
+
+    @property
+    def httpx(self):
+        return {
+            "http://": self._proxy.uri,
+            "https://": self._proxy.uri
+        }
+
+    @property
+    def pyrogram(self):
+        return {
+            "scheme": self._proxy.scheme,
+            "hostname": self._proxy.host,
+            "port": self._proxy.port,
+            "username": self._proxy.username,
+            "password": self._proxy.password
+        }
+
